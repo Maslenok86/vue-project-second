26e6d831cca6e54d84148176ca7d42e51a0b0e22
Anastasia <86nootka36@gmail.com>
2022-09-04 23:04:28 +0300

    Header and Promotion

---
+++ src/components/Header.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <div class="header">
+    <div class="header-container">
+      <div class="header-logos">
+        <img
+            src="https://thumb.tildacdn.com/tild3964-3431-4230-b561-336536616165/-/resize/169x/-/format/webp/logo_okla.png">
+        <img src="https://thumb.tildacdn.com/tild6639-3035-4837-a661-393134623532/-/resize/122x/-/format/webp/_.png">
+      </div>
+      <div class="header-menu">
+        <div class="header-menu-element">О квартале</div>
+        <div class="header-menu-element">Квартиры</div>
+        <div class="header-menu-element">Как купить</div>
+        <div class="header-menu-element">Контакты</div>
+      </div>
+      <div class="header-phone">
+        8 800 555 35 35
+      </div>
+      <div class="header-button-request">
+        Заказать звонок
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 100px;
+  /*border: 2px solid rosybrown;*/
+  font: 14pt "Book Antiqua";
+}
+
+.header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  /*border: 2px solid rosybrown;*/
+  width: 1200px;
+}
+
+.header-logos {
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  align-items: center;
+  width: 310px;
+  justify-content: space-between;
+  /*border: 2px solid rosybrown;*/
+}
+
+.header-menu {
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  /*border: 2px solid rosybrown;*/
+}
+
+.header-menu-element {
+  margin: 0 10px 0 10px;
+}
+
+.header-phone {
+  align-self: center;
+  /*border: 2px solid rosybrown;*/
+  font-weight: bold;
+}
+
+.header-button-request {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  border: 2px solid rosybrown;
+  border-radius: 40px;
+  height: 40px;
+  width: 180px;
+  background-color: antiquewhite;
+}
+</style>
---
+++ src/components/Promotion.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  name: "Promotion"
+}
+</script>
+
+<template>
+  <div class="promotion">
+    <div class="promotion-grid-container">
+      <div class="promotion-grid-letter">Ж</div>
+      <div class="promotion-grid-line-horizontal"></div>
+      <div class="promotion-grid-letter">И</div>
+
+      <div></div>
+      <div class="promotion-grid-img">
+        <img class="promotion-img"
+             src="https://thumb.tildacdn.com/tild3735-3630-4830-b366-616433373762/-/format/webp/_1160x550__3_1.jpg">
+      </div>
+      <div class="promotion-grid-line-vertical"></div>
+
+      <div class="promotion-grid-letter">И</div>
+      <div class="promotion-grid-line-horizontal"></div>
+      <div class="promotion-grid-letter">В</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.promotion {
+  display: flex;
+  justify-content: center;
+  /*border: 2px solid rosybrown;*/
+  margin: 40px;
+}
+
+.promotion-grid-container {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  grid-template-rows: 50px 500px 50px;
+  grid-template-columns: 50px 1000px 50px;
+  /*border: 2px solid red;*/
+  font: 30pt "Book Antiqua";
+  font-weight: bold;
+
+}
+
+.promotion-grid-letter {
+  /*border: 1px solid blue;*/
+}
+
+.promotion-grid-line-horizontal {
+  border: 2px solid #faab15;
+  height: 0;
+  width: 996px;
+}
+
+.promotion-grid-line-vertical {
+  border: 2px solid #faab15;
+  height: 496px;
+  width: 0;
+}
+
+.promotion-grid-img {
+  display: flex;
+  align-items: center;
+  /*border: 1px solid blue;*/
+}
+
+.promotion-img {
+  width: 100%;
+}
+</style>
